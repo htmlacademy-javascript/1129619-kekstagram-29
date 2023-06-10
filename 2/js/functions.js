38a3014e397323b393c9aafec0d7a4b5bb5bc60c
Keks <keks@htmlacademy.ru>
2023-06-10 21:57:56 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,21 @@
+const getStringLength = (text, maxLength) => text.length <= maxLength;
+
+const checkPalindrome = (text) => {
+  const firsText = text.replaceAll(' ', '').toLowerCase();
+  let secondText = '';
+  for (let i = firsText.length - 1; i >= 0; i--) {
+    secondText += firsText[i];
+  }
+  return firsText === secondText;
+};
+
+const getNumber = (material) => {
+  let result = '';
+  for (const el of material.toString()) {
+    // !Number.isNaN(parseInt(el, 10)) ? result += el : null;
+    if (!Number.isNaN(parseInt(el, 10))) {
+      result += el;
+    }
+  }
+  return result !== 0 ? result : NaN;
+};
