2006e5d3cef012fa49f85d4e733abdad22dbe493
Anatolii <volotovskiy111@gmail.com>
2023-07-09 21:53:25 +0300

    Добавляет валидацию форм

--- js/forms.js
+++ js/forms.js
@@ -1,10 +1,22 @@
+import { isEscapeKey } from './util.js';
+
 const buttonUploadFile = document.getElementById('upload-file');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadForm = document.querySelector('.img-upload__form');
 const body = document.querySelector('body');
 const imgUploadCancel = document.querySelector('#upload-cancel');
+const textHashtags = document.querySelector('.text__hashtags');
+const textDescription = document.querySelector('.text__description');
 
-new Pristine(imgUploadForm);
+const regular = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+
+const prestine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper--invalid',
+  successClass: 'img-upload__field-wrapper--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'p',
+});
 
 buttonUploadFile.addEventListener('change', () => {
   imgUploadOverlay.classList.remove('hidden');
@@ -16,4 +28,45 @@ const onCloseForm = () => {
   body.classList.remove('modal-open');
 };
 
+const onPopupEscPress = function (evt) {
+  if (!(textHashtags === document.activeElement) || !(textDescription === document.activeElement)) {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      onCloseForm();
+    }
+  }
+};
+
+const cheskValidHashtag = () => {
+  const hashtagArr = textHashtags.value.split(' ');
+  hashtagArr.forEach((hashtag) => {
+    const testOfValid = ((hashtag[0] === '#') && (1 < hashtag.length) && (hashtag.length <= 20) && (regular.test(hashtag)));
+    return testOfValid;
+  });
+};
+
+const checkCountHashtag = () => {
+  const hashtagArr = textHashtags.value.split(' ');
+  return (hashtagArr.length <= 5);
+};
+
+const checkRepeatingGashtag = () => {
+  const hashtagArr = textHashtags.value.split(' ');
+  hashtagArr.filter((number, index, numbers) => {
+    const result = numbers.indexOf(number) !== index;
+    return(!result);
+  });
+};
+
+prestine.addValidator(textHashtags, cheskValidHashtag, 'Введён невалидный хэш-тег');
+prestine.addValidator(textHashtags, checkCountHashtag, 'Превышено количество хэш-тегов');
+prestine.addValidator(textHashtags, checkRepeatingGashtag, 'Хэш-теги повторяются');
+
+
+imgUploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  prestine.validate();
+});
+
 imgUploadCancel.addEventListener('click', onCloseForm);
+document.addEventListener('keydown', onPopupEscPress);
